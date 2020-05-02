8ad88dd01da5259407a2f64201abb3c1479945ba
MaxGraey <maxgraey@gmail.com>
2020-05-02 22:38:24 +0300

    improve html meta & code styles

--- interference/assembly/index.ts
+++ interference/assembly/index.ts
@@ -11,22 +11,24 @@ function set(x: i32, y: i32, v: f32): void {
 function distance(x1: i32, y1: i32, x2: f32, y2: f32): f32 {
   var dx = <f32>x1 - x2;
   var dy = <f32>y1 - y2;
-  return sqrt(dx * dx + dy * dy);
+  return Mathf.sqrt(dx * dx + dy * dy);
 }
 
 export function update(tick: f32): void {
-  var hw = <f32>width * 0.5,
-      hh = <f32>height * 0.5;
+  var w = <f32>width;
+  var h = <f32>height;
+  var hw = w * 0.5,
+      hh = h * 0.5;
   var cx1 = (Mathf.sin(tick * 2) + Mathf.sin(tick      )) * hw * 0.3 + hw,
       cy1 = (Mathf.cos(tick)                            ) * hh * 0.3 + hh,
       cx2 = (Mathf.sin(tick * 4) + Mathf.sin(tick + 1.2)) * hw * 0.3 + hw,
       cy2 = (Mathf.sin(tick * 3) + Mathf.cos(tick + 0.1)) * hh * 0.3 + hh;
-  var res = <f32>48 / max(<f32>width, <f32>height);
+  var res = <f32>48 / Mathf.max(w, h);
   var y = 0;
   do {
     let x = 0;
     do {
-      set(x, y, abs(
+      set(x, y, Mathf.abs(
         Mathf.sin(distance(x, y, cx1, cy1) * res) +
         Mathf.sin(distance(x, y, cx2, cy2) * res)
       ) * 120);
