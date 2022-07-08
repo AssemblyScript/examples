9b1c2840ab67d6174b54fba1acb8512cebe50217
MaxGraey <maxgraey@gmail.com>
2022-07-08 14:43:07 +0300

    better postprocess

--- n-body/scripts/postprocess.js
+++ n-body/scripts/postprocess.js
@@ -3,6 +3,11 @@ const path = require("path");
 
 const filename = path.join(__dirname, "..", "build" , "index.js");
 const source = fs.readFileSync(filename, { encoding: "utf8" });
-// remove uncheckeds
-const target = source.replace(/unchecked/g, "")
-fs.writeFileSync(filename, target);
+
+function removeUnchecked(str) {
+  return str
+    .replace(/unchecked\((.*?)\)/g, (_, _1) => _1)
+    .replace(/unchecked\((.*?)\)/g, (_, _1) => _1);
+}
+
+fs.writeFileSync(filename, removeUnchecked(source));
