30dedf0a05e81a744d5d5f15b6861b736d5aedd1
Congcong Cai <congcongcai0907@163.com>
2022-08-12 01:50:29 +0800

    Migrate to 0.20.x version for all examples (#22)

--- transform/README.md
+++ transform/README.md
@@ -9,10 +9,6 @@ Instructions
 * [JavaScript transform](./mytransform.js)<br />
   An ES6 JavaScript transform. Recommended.
 
-* [TypeScript transform](./mytransform.ts)<br />
-  A transform written in TypeScript. This works currently because the compiler
-  utilizes ts-node anyway, so can as well transpile the transform to ES6.
-
 See [package.json](./package.json) on how to utilize the transforms with `asc`.
 You may also run
 
--- transform/mytransform.mjs
+++ transform/mytransform.mjs
@@ -1,17 +1,21 @@
-const { Transform } = require("assemblyscript/cli/transform");
-const { SourceKind, ElementKind } = require("assemblyscript");
-const binaryen = require("binaryen");
+import * as assemblyscript from "assemblyscript";
+import { Transform } from "assemblyscript/transform";
+import binaryen from "binaryen";
 
 class MyTransform extends Transform {
   afterParse(parser) {
     this.log("[mytransform.js] afterParse called, baseDir = " + this.baseDir);
     var sources = this.program.sources;
-    sources.forEach(source => this.log("  " + source.internalPath + " [" + SourceKind[source.sourceKind] + "]"));
+    sources.forEach((source) =>
+      this.log("  " + source.internalPath + " [" + assemblyscript.SourceKind[source.sourceKind] + "]")
+    );
   }
   afterInitialize(program) {
     this.log("[mytransform.js] afterInitialize called");
     var elements = program.elementsByName;
-    elements.forEach(element => this.log("  " + element.internalName + " [" + ElementKind[element.kind] + "]"));
+    elements.forEach((element) =>
+      this.log("  " + element.internalName + " [" + assemblyscript.ElementKind[element.kind] + "]")
+    );
   }
   afterCompile(asModule) {
     this.log("[mytransform.js] afterCompile called");
@@ -20,4 +24,4 @@ class MyTransform extends Transform {
   }
 }
 
-module.exports = MyTransform;
+export default MyTransform;
