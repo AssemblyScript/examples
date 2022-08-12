2cb618983f2eb5f07fbb26d2241d175933726082
MaxGraey <maxgraey@gmail.com>
2022-08-12 13:07:25 +0300

    update

--- mandelbrot/index.js
+++ mandelbrot/index.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
-const compiled = new WebAssembly.Module(fs.readFileSync(__dirname + "/build/optimized.wasm"));
-const imports = { JSMath: Math };
+const compiled = new WebAssembly.Module(fs.readFileSync(__dirname + "/build/release.wasm"));
+const imports = {
+  env: {
+    "Math.log": Math.log,
+    "Math.log2": Math.log2,
+  }
+};
 Object.defineProperty(module, "exports", {
   get: () => new WebAssembly.Instance(compiled, imports).exports
 });
